f1dade7d17815e9d380c88fa7279cd767ea6a8aa
Ruslan Tsialevich <tsialrus@gmail.com>
2023-06-10 17:12:50 +0300

    Add project files.

---
+++ emptySQLProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace emptySQLProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "(localdb)\\MSSQLLocalDB";
+                builder.InitialCatalog = "fakeDB";
+                builder.UserID = "sa1";
+                builder.Password = "123456";
+
+                Console.WriteLine("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
+        }
+    }
+}
